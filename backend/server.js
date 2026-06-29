7ead863292b216e6a171112be56221ce0793dbc9
Maheshwaran <mageshedu77@gmail.com>
2026-06-29 23:35:55 +0530

    Plant Dashboard bug fixed and enchanced

--- backend/server.js
+++ backend/server.js
@@ -182,6 +182,6 @@ process.on('SIGINT', async () => {
   console.log('🛑 MongoDB connection closed'); 
   process.exit(0);
 });
-
+  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
